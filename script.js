9a987c5325351a0785f1f4f968235a16438b243d
hardikk04 <hardik04062003@gmail.com>
2024-02-02 15:46:23 +0530

    page 3 done

--- script.js
+++ script.js
@@ -50,6 +50,170 @@ tl.to(".main", {
     start: "top 100%",
     end: "top 90%",
     scrub: 1,
-    markers: true,
+    // markers: true,
   },
+});
+
+tl.to("nav", {
+  color: "#000",
+  borderBottom: "1px solid #0000003e",
+  scrollTrigger: {
+    scroller: ".main",
+    trigger: ".page2",
+    start: "top 100%",
+    end: "top 90%",
+    scrub: 1,
+    // markers: true,
+  },
+});
+
+tl.from(".page2-left-text", {
+  y: 300,
+  scrollTrigger: {
+    scroller: ".main",
+    trigger: ".page2",
+    start: "top 60%",
+    end: "top 0%",
+    scrub: 1,
+    // markers: true,
+  },
+});
+
+tl.from(".page2-left-text h3", {
+  opacity: 0,
+  scrollTrigger: {
+    scroller: ".main",
+    trigger: ".page3",
+    start: "top 0",
+    end: "top -10%",
+    scrub: 1,
+    // markers: true,
+  },
+});
+
+tl.from(".page2-right-text", {
+  y: 600,
+  scrollTrigger: {
+    scroller: ".main",
+    trigger: ".page2",
+    start: "top 0",
+    end: "top -100%",
+    scrub: 1,
+    // markers: true,
+    pin: true,
+  },
+});
+
+tl.from(".page3-elem1-img", {
+  height: "50vh",
+  scrollTrigger: {
+    scroller: ".main",
+    trigger: ".page3-elem1-img",
+    start: "top 70%",
+    end: "top 0%",
+    scrub: 1,
+    // markers: true,
+  },
+});
+
+tl.from(".page3-elem2-img", {
+  height: "50vh",
+  scrollTrigger: {
+    scroller: ".main",
+    trigger: ".page3-elem2-img",
+    start: "top 70%",
+    end: "top 0%",
+    scrub: 1,
+    // markers: true,
+  },
+});
+
+tl.from(".page3-elem3-img", {
+  height: "50vh",
+  scrollTrigger: {
+    scroller: ".main",
+    trigger: ".page3-elem3-img",
+    start: "top 10%",
+    end: "top -50%",
+    scrub: 1,
+    // markers: true,
+  },
+});
+
+const page3Elem = document.querySelector(".page3-elem1-img");
+const page3ImgCurosr = document.querySelector(".page3-img-cursor1");
+
+page3Elem.addEventListener("mouseenter", () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  // Set the background color
+  page3ImgCurosr.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
+
+page3Elem.addEventListener("mousemove", function (dets) {
+  gsap.to(".page3-img-cursor1", {
+    opacity: 1,
+    left: dets.x - 350,
+    top: dets.y - 250,
+  });
+});
+
+page3Elem.addEventListener("mouseleave", function (dets) {
+  gsap.to(".page3-img-cursor1", {
+    opacity: 0,
+  });
+});
+
+const page3Elem2 = document.querySelector(".page3-elem2-img");
+const page3ImgCurosr2 = document.querySelector(".page3-img-cursor2");
+
+page3Elem2.addEventListener("mouseenter", () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  // Set the background color
+  page3ImgCurosr2.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
+
+page3Elem2.addEventListener("mousemove", function (dets) {
+  gsap.to(".page3-img-cursor2", {
+    opacity: 1,
+    left: dets.x - 350,
+    top: dets.y - 250,
+  });
+});
+
+page3Elem2.addEventListener("mouseleave", function (dets) {
+  gsap.to(".page3-img-cursor2", {
+    opacity: 0,
+  });
+});
+
+const page3Elem3 = document.querySelector(".page3-elem3-img");
+const page3ImgCurosr3 = document.querySelector(".page3-img-cursor3");
+
+page3Elem3.addEventListener("mouseenter", () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  // Set the background color
+  page3ImgCurosr3.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
+
+page3Elem3.addEventListener("mousemove", function (dets) {
+  gsap.to(".page3-img-cursor3", {
+    opacity: 1,
+    left: dets.x - 350,
+    top: dets.y - 250,
+  });
+});
+
+page3Elem3.addEventListener("mouseleave", function (dets) {
+  gsap.to(".page3-img-cursor3", {
+    opacity: 0,
+  });
 });
